ec51c2c3c3a8236eec9dbe20e67bd2ea44a635f7
Starship100 <jimmy.widlund@outlook.com>
2026-01-18 19:32:39 +0100

    1a,1b,2a

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ first_number = int(input("Skriv in ett heltal: "))
 print(first_number)
 
 # 1b
-second_number = input("Skriv in ett till heltal: ")
-second_int = int(second_number)
-print(second_int)
-sum = first_number + second_int
+second_number = int(input("Skriv in ett till heltal: "))
+#second_int = int(second_number)
+print(second_number)
+sum = first_number + second_number
 print(sum)
 
 # 2a
@@ -22,10 +22,10 @@ slut_pris = int(pris_jacka * rea_procent / 100)
 print("Du behöver betala: " + str(slut_pris) + "kr.")
 
 #  2b
-skriv_procent = input("Skriv in procentsats: ")
-skriv_procent_int = float(skriv_procent)
+skriv_procent = int(input("Skriv in procentsats: "))
+#skriv_procent_int = float(skriv_procent)
 pris_jacka = 2000
 # procent_av_pris = float(pris_jacka * skriv_procent_int / 100)
-procent_av_pris = pris_jacka * skriv_procent_int / 100
+procent_av_pris = pris_jacka * skriv_procent / 100
 slut_pris = pris_jacka - procent_av_pris
 print("Du behöver betala: " + str(slut_pris) + "kr.")
