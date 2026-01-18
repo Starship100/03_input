0eedf4fe54b683b350a50a0ddc9f337fd572f877
Starship100 <jimmy.widlund@outlook.com>
2026-01-18 18:33:16 +0100

    1a,1b,2a

--- main.py
+++ main.py
@@ -21,3 +21,10 @@ slut_pris = int(pris_jacka * rea_procent / 100)
 print("Du behöver betala: " + str(slut_pris) + "kr.")
 
 # 2b
+skriv_procent = input("Skriv in procentsats: ")
+skriv_procent_int = float(skriv_procent)
+pris_jacka = 2000
+# procent_av_pris = float(pris_jacka * skriv_procent_int / 100)
+procent_av_pris = pris_jacka * skriv_procent_int / 100
+slut_pris = pris_jacka - procent_av_pris
+print("Du behöver betala: " + str(slut_pris) + "kr.")
