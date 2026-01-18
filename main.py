4a91dfc8bbafa5701522a2b57355928ea3a25820
Starship100 <jimmy.widlund@outlook.com>
2026-01-18 17:29:23 +0100

    1a,1b,2a

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+# 1a
+first_number = input("Skriv in ett heltal: ")
+first_int = int(first_number)
+print(first_int)
+
+# 1b
+second_number = input("Skriv in ett till heltal: ")
+second_int = int(second_number)
+print(second_int)
+sum = first_int + second_int
+print(sum)
+
+# 2a
+pris_jacka = 2000
+rea_procent = 75.0
+slut_pris = int(pris_jacka * rea_procent / 100)
+#sum = int(slut_pris / 100)
+#print("Du behöver betala: " + str(sum) + "kr.")
+print("Du behöver betala: " + str(slut_pris) + "kr.")
+
+# 2b
